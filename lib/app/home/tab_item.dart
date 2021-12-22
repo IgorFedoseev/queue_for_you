f37739a76cafb69797c35e6a8b111da4bdbcc114
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2021-12-23 01:58:56 +0300

    Adding work schedules to FireStore

--- lib/app/home/tab_item.dart
+++ lib/app/home/tab_item.dart
@@ -14,10 +14,10 @@ class TabItemData {
   final IconData icon;
 
   static const Map<TabItem, TabItemData> allTabs = {
-    TabItem.profile: TabItemData(label: 'Профиль', icon: Icons.person),
-    TabItem.jobs: TabItemData(label: 'Услуги', icon: Icons.business_center),
-    TabItem.schedule: TabItemData(label: 'График', icon: Icons.more_time),
-    TabItem.entries: TabItemData(label: 'Записи', icon: Icons.view_headline),
-    TabItem.contacts: TabItemData(label: 'Контакты', icon: Icons.people_sharp),
+    TabItem.profile: TabItemData(label: 'ПРОФИЛЬ', icon: Icons.person),
+    TabItem.jobs: TabItemData(label: 'УСЛУГИ', icon: Icons.business_center),
+    TabItem.schedule: TabItemData(label: 'ГРАФИК', icon: Icons.more_time),
+    TabItem.entries: TabItemData(label: 'ЗАПИСИ', icon: Icons.view_headline),
+    TabItem.contacts: TabItemData(label: 'КОНТАКТЫ', icon: Icons.people_sharp),
   };
 }
